77222c9b8e09583f3f37f62df7582f09cf9ae1f6
DimitrijeM <dimitrijemilenkovic.dm@gmail.com>
2020-06-23 22:12:15 +0200

    adaptive learning rate by cauchy

--- linear_regression/LinearRegression.py
+++ linear_regression/LinearRegression.py
@@ -3,7 +3,7 @@ import numpy as np
 import sys
 
 class LinearRegression:
-	def __init__(self, learning_rate=0.01, regularization_param=0.0, gradient_type='base', learning_rate_factor=1):
+	def __init__(self, learning_rate=0.01, regularization_param=0.0, gradient_type='base'):
 		self.learning_rate = learning_rate
 		self.regularization_param = regularization_param
 		self.gradient_type = gradient_type
@@ -14,7 +14,6 @@ class LinearRegression:
 
 		self.fit = self.__fit_with_stochastic_gd if gradient_type == 'SGD' else self.__fit_with_gradient_descent
 
-		self.learning_rate_factor = learning_rate_factor
 
 	@staticmethod
 	def get_np_array(x):
@@ -43,21 +42,49 @@ class LinearRegression:
 	def normalize(self, x):
 		return (x - self.x_mean) / self.x_std
 
-	# todo: very naive method, results in more iteration till convergence, find better solution and remove it. source: https://stats.stackexchange.com/questions/46130/determine-the-optimum-learning-rate-for-gradient-descent-in-linear-regression
-	def adapt_learning_rate_by_factor(self, prior_mse, mse):
+	@staticmethod
+	def calculate_mean_squared_error(errors, error_regularizator=0):
+		return (errors.T.dot(errors) + error_regularizator) / errors.shape[0]
+
+	@staticmethod
+	def predict_and_evaluate(x, y, w):
+		predicted = x.dot(w.T)
+		errors = predicted - y
+		mse = LinearRegression.calculate_mean_squared_error(errors, 0)
+		return mse
+
+	@staticmethod
+	def adapt_learning_rate_const(learning_rate):
+		return learning_rate
+
+	# todo: very naive method, results in more iteration till convergence, find better solution and remove it.
+	#  source: https://stats.stackexchange.com/questions/46130/determine-the-optimum-learning-rate-for-gradient-descent-in-linear-regression
+	@staticmethod
+	def adapt_learning_rate_by_factor(x, y, w, prior_w, learning_rate, learning_rate_factor):
+		prior_mse = LinearRegression.predict_and_evaluate(x, y, prior_w)
+		mse = LinearRegression.predict_and_evaluate(x, y, w)
 		if mse <= prior_mse:
-			self.learning_rate = self.learning_rate * self.learning_rate_factor
+			return learning_rate * learning_rate_factor
 		else:
-			self.learning_rate = self.learning_rate / self.learning_rate_factor
+			return learning_rate / learning_rate_factor
 
-	# todo: not good, need to read well http://www.onmyphd.com/?p=gradient.descent
-	def adapt_learning_rate_by_barzilai(self, prior_gradients, gradients, prior_lr, lr):
-		delta_g = gradients - prior_gradients
-		delta_x = lr - prior_lr
-		new_lr = (delta_g * delta_x) / (delta_g**2)
-		return new_lr, lr
+	@staticmethod
+	def adapt_learning_rate_by_cauchy(x, y, w, gradients, possible_lr=[0.001, 0.005, 0.1, 0.2, 0.3, 0.5]):
+		values = np.array([LinearRegression.predict_and_evaluate(x, y, w-lr*gradients) for lr in possible_lr])
+		selected_lr = possible_lr[np.argmin(values)]
+		# print(possible_lr)
+		# print(list(values))
+		# print(f'selected {selected_lr}')
+		return selected_lr
 
-	def __fit_with_gradient_descent(self, x, y, epochs, log_epoch=1000):
+	# source: http://www.onmyphd.com/?p=gradient.descent
+	@staticmethod
+	def adapt_learning_rate_by_barzilai(delta_w, delta_gradients):
+		selected_lambda = abs((delta_gradients.dot(delta_w.T)) / delta_gradients.dot(delta_gradients.T))
+		return selected_lambda
+
+	def __fit_with_gradient_descent(self, x, y, epochs, log_epoch=1000, gradients_to_stop=0, mse_to_stop=0,
+									adapt_learning_rate=None, learning_rate_factor=1):
 		x = LinearRegression.get_np_array(x)
 		y = LinearRegression.get_np_array(y)
 
@@ -68,25 +95,39 @@ class LinearRegression:
 
 		n, m = x.shape
 		self.w = np.random.random((1, m))
-		prior_lr = 0
-		gradients = np.zeros((1, m))
+		gradients = np.zeros(m)
 		for i in range(epochs):
+			prior_gradients = gradients
+			prior_w = self.w
+
 			predicted = x.dot(self.w.T)
 			gradient_regularizator, error_regularizator = LinearRegression.get_ridge_gradient_and_error_regulazator(
 				self.regularization_param, self.w, n)
 			errors = predicted - y + gradient_regularizator
-			prior_gradients = gradients
 			gradients = errors.T.dot(x) / n
+
 			self.w = self.w - self.learning_rate * gradients
 
-			mean_squared_error = (errors.T.dot(errors) + error_regularizator) / n
+			# adaptive lr
+			if adapt_learning_rate == 'cauchy':
+				self.learning_rate = self.adapt_learning_rate_by_cauchy(x, y, self.w, gradients)
+			# elif adapt_learning_rate == 'barzilai':
+			# 	self.learning_rate = self.adapt_learning_rate_by_barzilai(self.w - prior_w, gradients - prior_gradients)
+			# elif adapt_learning_rate == 'factor':
+			# 	self.learning_rate = self.adapt_learning_rate_by_factor(x, y, self.w, prior_w,
+			# 															self.learning_rate, learning_rate_factor)
+
+
+			mean_squared_error = LinearRegression.calculate_mean_squared_error(errors, error_regularizator)
 			gradients_norm = abs(gradients).sum()
-			# self.learning_rate, prior_lr = self.adapt_learning_rate_by_barzilai(prior_gradients, gradients, prior_lr, self.learning_rate)
 
 			if i % log_epoch == 0:
 				print(f"Iter: {i}, gradients norm: {gradients_norm}, mse: {mean_squared_error}")
+			if gradients_norm < gradients_to_stop or float(mean_squared_error) < mse_to_stop:
+				break
 
-	def __fit_with_stochastic_gd(self, x, y, epochs, log_epoch=1000):
+	def __fit_with_stochastic_gd(self, x, y, epochs, log_epoch=1000, gradients_to_stop=0, mse_to_stop=0,
+								 adapt_learning_rate=None, learning_rate_factor=1):
 		x = LinearRegression.get_np_array(x)
 		y = LinearRegression.get_np_array(y)
 
@@ -113,6 +154,8 @@ class LinearRegression:
 				gradients_norm = abs(gradients).sum()
 				if i % log_epoch == 0 and row_index % 100 == 0:
 					print(f"Iter: {i}, row: {row_index}, gradients norm: {gradients_norm}, mse: {mean_square_error}")
+				if gradients_norm < gradients_to_stop or mean_square_error < mse_to_stop:
+					break
 
 	def predict(self, x):
 		x = LinearRegression.get_np_array(x)
@@ -133,9 +176,11 @@ if __name__ == "__main__":
 	y = data.iloc[:, [-1]]
 	x = data.iloc[:, 0:-1]
 
-	model_lr = LinearRegression(0.01, regularization_param=1e-2, gradient_type='batch')
-	# model_lr = LinearRegression(0.001, regularization_param=1e-3, gradient_type='SGD')
-	model_lr.fit(x, y, epochs=int(1e4), log_epoch=100)
+	# model_lr = LinearRegression(0.01, regularization_param=1e-3, gradient_type='SGD')
+	# model_lr.fit(x, y, epochs=int(1e3), log_epoch=100, gradients_to_stop=1e-6)
+
+	model_lr = LinearRegression(0.1, regularization_param=1e-2, gradient_type='batch')
+	model_lr.fit(x, y, epochs=int(1e4), log_epoch=100, gradients_to_stop=1e-6, adapt_learning_rate='cauchy')
 	prediction = model_lr.predict(x)
 	data['predicted'] = prediction
 	print(data)
